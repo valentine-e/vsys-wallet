6e3ad3f9d37459de20f54e73104517709a708c8f
easy957 <easy957@gmail.com>
2022-01-26 00:39:14 +0200

    wallet custom select

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,9 +1,11 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-  const userViewport = window.innerWidth;
+
   
   menuBtnRef.addEventListener("click", () => {
     mobileMenuRef.classList.toggle("is-open");
   });
+
+  
 })();
